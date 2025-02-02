5497776477b2330dbfed395ee6721d8b6216677c
Justin <justinickler@gmail.com>
2025-02-02 10:34:44 -0500

    creating DB and first model

---
+++ Data/Character.cs
@@ -0,0 +1,13 @@
+﻿namespace AzurLaneLoreWiki.Data
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Faction { get; set; }
+        public string? Prefix { get; set; }// e.g. HMS, USS, IJN, etc. can't just go off faction due to cases like IB with SMS/KMS
+        public string? Class { get; set; }
+        public string? Description { get; set; }
+        public string? Icon { get; set; }
+    }
+}
